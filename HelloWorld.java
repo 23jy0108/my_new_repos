fcb277c9db9060b34a588ef63ee1fda378abf047
23jy0108 <23jy0108@jec.ac.jp>
2024-10-16 15:27:55 +0900

    日本電子専門学校の一行を追加

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld{
   public static void main(String[] args){
-    System.out.print("日本電子");
+    System.out.println("HelloWorld");
+    System.out.println("日本電子専門学校");
   }
   
 }
